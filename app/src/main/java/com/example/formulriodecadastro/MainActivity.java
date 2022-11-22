e85be67312deec9484f6b65090a1725bd813e781
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2022-11-21 21:28:46 -0300

    criando os id

--- app/src/main/java/com/example/formulriodecadastro/MainActivity.java
+++ app/src/main/java/com/example/formulriodecadastro/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.formulriodecadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nome,idade;
+    private RadioButton masculino,feminino;
+    private TextView resultadoNome,resultadoIdade,resultadoSexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = findViewById()
     }
 }
