7d56810cd7d2f9c90aebca3a2cc0b30a2b8e8855
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2022-11-22 21:13:58 -0300

    criando os id

--- app/src/main/java/com/example/formulriodecadastro/MainActivity.java
+++ app/src/main/java/com/example/formulriodecadastro/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.formulriodecadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nome = findViewById()
+        nome = findViewById(R.id.editNome);
+        idade = findViewById(R.id.editIdade);
+        masculino = findViewById(R.id.radioButtonMasculino);
+        feminino = findViewById(R.id.radioButtonFeminino);
+        resultadoNome = findViewById(R.id.resultadoNome);
+        resultadoIdade = findViewById(R.id.resultadoIdade);
+        resultadoSexo = findViewById(R.id.resultadoSexo);
+        Button botaoCadastrar = findViewById(R.id.bt_Cadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String editNome = nome.getText().toString();
+                String editIdade = idade.getText().toString();
+
+                if (editNome.isEmpty() || editIdade.isEmpty()){
+                    Snackbar.make(view, "Preencha o nome e a idade",Snackbar.LENGTH_SHORT).show();
+                }else{
+
+            }
+        });
     }
 }
